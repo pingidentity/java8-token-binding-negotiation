3da31fd6aeea633b87b0732a1e046dda058dce75
Brian Campbell <brian.d.campbell@gmail.com>
2017-12-20 12:57:48 -0700

    add license file headers to the new source files added in support of token binding (as well as the TB dependencies on support of Keying Material Exporters [RFC 5705] and Extended Master Secret [RFC7627])

--- src/main/java/sun/security/ssl/ExtendedMasterSecretExtension.java
+++ src/main/java/sun/security/ssl/ExtendedMasterSecretExtension.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2017, Ping Identity Corp. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Ping Identity designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ */
+
 // -- token binding etc. changes begin --
 package sun.security.ssl;
 
